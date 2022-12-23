886ee0956b475dc72c8328d90c67c86adf547d7e
A-m-a-d-o <pedrocastroamado@gmail.com>
2022-12-23 14:48:39 +0000

    I2C working and alarms working

--- Car Cooling/main (3).c
+++ Car Cooling/main (3).c
@@ -9,14 +9,12 @@
 #include "driverlib/sysctl.h"
 #include "driverlib/gpio.h"
 #include "driverlib/pin_map.h"
-
+#include <math.h>
 #define SLAVE_ADDR              0x48
 #define REG_TEMP_ADDR           0x00
 #define REG_CONFIG_ADDR         0x01
-#define REG_TLOW_ADDR           0x02
-#define REG_THIGH_ADDR          0x03
-
-//-----------------------Prototipos das funcoes-----------------------//
+#define REG_TLOW           0x02
+#define REG_TMAX         0x03
 
 void I2CInit(void);
 void I2CWrite(uint8_t reg_addr, uint16_t data);
@@ -58,6 +56,27 @@ void config_i2c(void){
     I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_SEND_FINISH);
     while(I2CMasterBusy(I2C1_BASE));
 }
+
+void i2c_data_write(uint16_t data, uint8_t reg)
+{
+    I2CMasterSlaveAddrSet(I2C1_BASE, SLAVE_ADDR, false);
+
+    I2CMasterDataPut(I2C1_BASE, reg);
+    I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_SEND_START);
+    while(I2CMasterBusy(I2C1_BASE));
+
+    I2CMasterDataPut(I2C1_BASE, (data >> 8));
+    I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_SEND_CONT);
+    while(I2CMasterBusy(I2C1_BASE));
+
+    I2CMasterDataPut(I2C1_BASE, (data & 0x00FF));
+    I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_SEND_FINISH);
+    while(I2CMasterBusy(I2C1_BASE));
+
+}
+
+
+
 
 uint16_t I2CRead(uint8_t reg_addr)
 {
@@ -107,24 +126,34 @@ float temp_get (void){
     return aux * 0.25;
 }
 
-void set_temp_max(float temp_max)
+uint8_t set_temp(float temp_max, uint8_t reg)
 {
+    const float precision = 0.0625;
+    float math_aux;
     uint16_t aux;
 
-    aux = temp_max / 0.0625;
 
-
-
-    I2CMasterSlaveAddrSet(I2C1_BASE, SLAVE_ADDR, false);
-
-    I2CMasterDataPut(I2C1_BASE, 0x03);
-    I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_SEND_START);
-    while(I2CMasterBusy(I2C1_BASE));
-
-    I2CMasterDataPut(I2C1_BASE, 0x6E);
-    I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_SEND_FINISH);
-    while(I2CMasterBusy(I2C1_BASE));
-
+    if (temp_max < 0 && temp_max > -128 )
+    {
+        temp_max = fabs(temp_max);
+        math_aux = fmod(temp_max, precision);
+        temp_max -= math_aux;
+        aux = temp_max / precision;
+        aux = (~aux) +1;
+        aux = aux << 4;
+        i2c_data_write(aux, reg);
+        return 1;
+    }
+    else if (temp_max > 0 && temp_max < 128)
+    {
+        math_aux = fmod(temp_max, precision);
+        temp_max -= math_aux;
+        aux = temp_max / precision;
+        aux = aux << 4;
+        i2c_data_write(aux, reg);
+        return 1;
+    }
+ return -1;
 }
 
 
@@ -137,10 +166,10 @@ int main(void)
 
         I2CInit();
         config_i2c();
+        set_temp(24, REG_TMAX);
+        set_temp(22, REG_TLOW);
         while(1){
-
         temp = temp_get();
-
     }
 
 	return 0;
