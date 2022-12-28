2c349821a4e2ffbf0900706d2bfb0c271ca01846
A-m-a-d-o <pedrocastroamado@gmail.com>
2022-12-28 11:57:09 +0000

    Actuator and Can project

--- Car Cooling/i2c.c
+++ Car Cooling/i2c.c
@@ -1,23 +1,41 @@
 /*
  * i2c.c
  *
- *  Created on: 15/12/2022
- *      Author: mac
+ *  Created on: 23/12/2022
+ *      Author: Pedro Amado
  */
 
-#include "i2c.h"
 
-void i2c_init(void)
-{
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include "inc/hw_i2c.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "inc/hw_gpio.h"
+#include "driverlib/i2c.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+#include "driverlib/pin_map.h"
+#include <math.h>
 
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_I2C1);     //Enables I2C1 peripheral
+
+
+#define SLAVE_ADDR              0x48
+#define REG_TEMP_ADDR           0x00
+#define REG_CONFIG_ADDR         0x01
+#define REG_TLOW           0x02
+#define REG_TMAX         0x03
+
+
+//initialize I2C module 1
+void I2CInit(void){
+
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_I2C1);
+
     SysCtlPeripheralReset(SYSCTL_PERIPH_I2C1);
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);    //Enables GPIO PORT A
 
-    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOA));  //Waits for the peripherals to initialize
-    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_I2C1));
-
-    //Configures pins for SCL and SDA
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
 
     GPIOPinConfigure(GPIO_PA6_I2C1SCL);
     GPIOPinConfigure(GPIO_PA7_I2C1SDA);
@@ -25,53 +43,119 @@ void i2c_init(void)
     GPIOPinTypeI2CSCL(GPIO_PORTA_BASE, GPIO_PIN_6);
     GPIOPinTypeI2C(GPIO_PORTA_BASE, GPIO_PIN_7);
 
-
-
-    I2CMasterInitExpClk(I2C1_BASE, SysCtlClockGet(), false);    //Setups the I2C1 clock frequency
-
+    I2CMasterInitExpClk(I2C1_BASE, SysCtlClockGet(), false);
 
 
 }
-void sensor_init(void)
-{
-    I2CMasterSlaveAddrSet(I2C1_BASE, TMP_101_ADD, false);       //Master sends the Slave Address
-    I2CMasterDataPut(I2C1_BASE, 0x01);                          //Sets the Pointer to the Configuration Register
+
+void config_i2c(void){
+
+    I2CMasterSlaveAddrSet(I2C1_BASE, SLAVE_ADDR, false);
+
+    I2CMasterDataPut(I2C1_BASE, 0x01);
     I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_SEND_START);
     while(I2CMasterBusy(I2C1_BASE));
-    I2CMasterDataPut(I2C1_BASE, 0x2E);                          //Setting the Resolution to 10 bits and continuous readings
+
+    I2CMasterDataPut(I2C1_BASE, 0x6E);
     I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_SEND_FINISH);
     while(I2CMasterBusy(I2C1_BASE));
 }
-void temp_get(void)
+
+void i2c_data_write(uint16_t data, uint8_t reg)
 {
+    I2CMasterSlaveAddrSet(I2C1_BASE, SLAVE_ADDR, false);
 
-    uint16_t temp;
+    I2CMasterDataPut(I2C1_BASE, reg);
+    I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_SEND_START);
+    while(I2CMasterBusy(I2C1_BASE));
 
+    I2CMasterDataPut(I2C1_BASE, (data >> 8));
+    I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_SEND_CONT);
+    while(I2CMasterBusy(I2C1_BASE));
 
-    I2CMasterSlaveAddrSet(I2C1_BASE, TMP_101_ADD, false);
-
-    I2CMasterDataPut(I2C1_BASE, 0x00);
-    SysCtlDelay(5000000);
-    I2CMasterDataPut(I2C1_BASE, 0x00);
-    I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_SINGLE_SEND);
-    while(I2CMasterBusBusy(I2C1_BASE));
-
-
-    I2CMasterSlaveAddrSet(I2C1_BASE, TMP_101_ADD, true);
-    temp = I2CMasterDataGet(I2C1_BASE) << 8;
-    I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_RECEIVE_START);
-    while(I2CMasterBusBusy(I2C1_BASE));
-
-    temp |=  I2CMasterDataGet(I2C1_BASE);
-    I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_RECEIVE_FINISH);
-    while(I2CMasterBusBusy(I2C1_BASE));
-
-
-
-    temp = temp >> 4;
-    return;
-
+    I2CMasterDataPut(I2C1_BASE, (data & 0x00FF));
+    I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_SEND_FINISH);
+    while(I2CMasterBusy(I2C1_BASE));
 
 }
 
 
+
+
+uint16_t I2CRead(uint8_t reg_addr)
+{
+    uint16_t data = 0;
+
+    I2CMasterSlaveAddrSet(I2C1_BASE, SLAVE_ADDR, false);
+
+    I2CMasterDataPut(I2C1_BASE, reg_addr);
+    I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_SINGLE_SEND);
+    while(I2CMasterBusy(I2C1_BASE));
+
+    I2CMasterSlaveAddrSet(I2C1_BASE, SLAVE_ADDR, true);
+
+    I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_RECEIVE_START);
+    while(I2CMasterBusy(I2C1_BASE));
+    data = I2CMasterDataGet(I2C1_BASE) << 8;
+
+
+    I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_RECEIVE_FINISH);
+    while(I2CMasterBusy(I2C1_BASE));
+    data |= I2CMasterDataGet(I2C1_BASE);
+
+    return data;
+}
+
+float temp_get (void){
+    uint16_t aux;
+
+
+
+
+
+    aux= I2CRead(0x00);
+
+    if((aux && 0x8000) == 0x8000)
+    {
+        aux &= 0x7FFF;
+
+        aux = aux >> 6;
+
+            return -(aux * 0.25);
+
+    }
+
+    aux = aux >> 6;
+
+    return aux * 0.25;
+}
+
+uint8_t set_temp(float temp_max, uint8_t reg)
+{
+    const float precision = 0.0625;
+    float math_aux;
+    uint16_t aux;
+
+
+    if (temp_max < 0 && temp_max > -128 )
+    {
+        temp_max = fabs(temp_max);
+        math_aux = fmod(temp_max, precision);
+        temp_max -= math_aux;
+        aux = temp_max / precision;
+        aux = (~aux) +1;
+        aux = aux << 4;
+        i2c_data_write(aux, reg);
+        return 1;
+    }
+    else if (temp_max > 0 && temp_max < 128)
+    {
+        math_aux = fmod(temp_max, precision);
+        temp_max -= math_aux;
+        aux = temp_max / precision;
+        aux = aux << 4;
+        i2c_data_write(aux, reg);
+        return 1;
+    }
+ return -1;
+}
--- Car Cooling/main (3).c
+++ Car Cooling/main (3).c
@@ -1,175 +1,64 @@
-#include <stdarg.h>
 #include <stdbool.h>
 #include <stdint.h>
-#include "inc/hw_i2c.h"
+#include "inc/hw_can.h"
+#include "inc/hw_ints.h"
 #include "inc/hw_memmap.h"
-#include "inc/hw_types.h"
-#include "inc/hw_gpio.h"
-#include "driverlib/i2c.h"
-#include "driverlib/sysctl.h"
+#include "driverlib/can.h"
 #include "driverlib/gpio.h"
+#include "driverlib/interrupt.h"
 #include "driverlib/pin_map.h"
-#include <math.h>
-#define SLAVE_ADDR              0x48
-#define REG_TEMP_ADDR           0x00
-#define REG_CONFIG_ADDR         0x01
-#define REG_TLOW           0x02
-#define REG_TMAX         0x03
-
-void I2CInit(void);
-void I2CWrite(uint8_t reg_addr, uint16_t data);
-uint16_t I2CRead(uint8_t reg_addr);
-void I2CWriteConf(uint8_t data);
-float ReadTemp (void);
-
-//-----------------------Definicao das funcoes-----------------------//
-
-//initialize I2C module 1
-void I2CInit(void){
-
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_I2C1);
-
-    SysCtlPeripheralReset(SYSCTL_PERIPH_I2C1);
-
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
-
-    GPIOPinConfigure(GPIO_PA6_I2C1SCL);
-    GPIOPinConfigure(GPIO_PA7_I2C1SDA);
-
-    GPIOPinTypeI2CSCL(GPIO_PORTA_BASE, GPIO_PIN_6);
-    GPIOPinTypeI2C(GPIO_PORTA_BASE, GPIO_PIN_7);
-
-    I2CMasterInitExpClk(I2C1_BASE, SysCtlClockGet(), false);
-
-
-}
-
-void config_i2c(void){
-
-    I2CMasterSlaveAddrSet(I2C1_BASE, SLAVE_ADDR, false);
-
-    I2CMasterDataPut(I2C1_BASE, 0x01);
-    I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_SEND_START);
-    while(I2CMasterBusy(I2C1_BASE));
-
-    I2CMasterDataPut(I2C1_BASE, 0x6E);
-    I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_SEND_FINISH);
-    while(I2CMasterBusy(I2C1_BASE));
-}
-
-void i2c_data_write(uint16_t data, uint8_t reg)
-{
-    I2CMasterSlaveAddrSet(I2C1_BASE, SLAVE_ADDR, false);
-
-    I2CMasterDataPut(I2C1_BASE, reg);
-    I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_SEND_START);
-    while(I2CMasterBusy(I2C1_BASE));
-
-    I2CMasterDataPut(I2C1_BASE, (data >> 8));
-    I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_SEND_CONT);
-    while(I2CMasterBusy(I2C1_BASE));
-
-    I2CMasterDataPut(I2C1_BASE, (data & 0x00FF));
-    I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_SEND_FINISH);
-    while(I2CMasterBusy(I2C1_BASE));
-
-}
-
-
-
-
-uint16_t I2CRead(uint8_t reg_addr)
-{
-    uint16_t data = 0;
-
-    I2CMasterSlaveAddrSet(I2C1_BASE, SLAVE_ADDR, false);
-
-    I2CMasterDataPut(I2C1_BASE, reg_addr);
-    I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_SINGLE_SEND);
-    while(I2CMasterBusy(I2C1_BASE));
-
-    I2CMasterSlaveAddrSet(I2C1_BASE, SLAVE_ADDR, true);
-
-    I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_RECEIVE_START);
-    while(I2CMasterBusy(I2C1_BASE));
-    data = I2CMasterDataGet(I2C1_BASE) << 8;
-
-
-    I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_RECEIVE_FINISH);
-    while(I2CMasterBusy(I2C1_BASE));
-    data |= I2CMasterDataGet(I2C1_BASE);
-
-    return data;
-}
-
-float temp_get (void){
-    uint16_t aux;
-
-
-
-
-
-    aux= I2CRead(0x00);
-
-    if((aux && 0x8000) == 0x8000)
-    {
-        aux &= 0x7FFF;
-
-        aux = aux >> 6;
-
-            return -(aux * 0.25);
-
-    }
-
-    aux = aux >> 6;
-
-    return aux * 0.25;
-}
-
-uint8_t set_temp(float temp_max, uint8_t reg)
-{
-    const float precision = 0.0625;
-    float math_aux;
-    uint16_t aux;
-
-
-    if (temp_max < 0 && temp_max > -128 )
-    {
-        temp_max = fabs(temp_max);
-        math_aux = fmod(temp_max, precision);
-        temp_max -= math_aux;
-        aux = temp_max / precision;
-        aux = (~aux) +1;
-        aux = aux << 4;
-        i2c_data_write(aux, reg);
-        return 1;
-    }
-    else if (temp_max > 0 && temp_max < 128)
-    {
-        math_aux = fmod(temp_max, precision);
-        temp_max -= math_aux;
-        aux = temp_max / precision;
-        aux = aux << 4;
-        i2c_data_write(aux, reg);
-        return 1;
-    }
- return -1;
-}
-
+#include "driverlib/sysctl.h"
+#include "driverlib/uart.h"
+#include "utils/uartstdio.h"
 
 //----------------------------Funcao main-----------------------------//
+volatile bool errFlag = 0; // transmission error flag
+
 
 int main(void)
+{
+    tCANMsgObject msg; // the CAN message object
+     unsigned int msgData = 200; // the message data is four bytes long which we can allocate as an int32
+    unsigned char *msgDataPtr = (unsigned char *)&msgData;
+
+
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
+
+    GPIOPinTypeGPIOInput(GPIO_PORTE_BASE, GPIO_PIN_4);
+    GPIOPinTypeGPIOOutput(GPIO_PORTE_BASE, GPIO_PIN_5);
+    GPIOPinConfigure(GPIO_PE4_CAN0RX);
+    GPIOPinConfigure(GPIO_PE5_CAN0TX);
+
+
+    GPIOPinTypeCAN(GPIO_PORTE_BASE, GPIO_PIN_4 | GPIO_PIN_5);
+
+
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_CAN0);
+
+
+    CANInit(CAN0_BASE);
+
+
+    #if defined(TARGET_IS_TM4C129_RA0) ||                                         \
+        defined(TARGET_IS_TM4C129_RA1) ||                                         \
+        defined(TARGET_IS_TM4C129_RA2)
+        CANBitRateSet(CAN0_BASE, ui32SysClock, 100000);
+    #else
+        CANBitRateSet(CAN0_BASE, SysCtlClockGet(), 100000);
+    #endif
+
+    CANEnable(CAN0_BASE);
+
+        msgData = 200;
+        msg.ui32MsgID =0x01;
+        msg.ui32MsgIDMask = 0;
+        msg.ui32Flags =0;
+        msg.ui32MsgLen = sizeof(msgDataPtr);
+        msg.pui8MsgData = msgDataPtr;
+
+    while(1)
     {
-
-        float temp;
-
-        I2CInit();
-        config_i2c();
-        set_temp(24, REG_TMAX);
-        set_temp(22, REG_TLOW);
-        while(1){
-        temp = temp_get();
+        CANMessageSet(CAN0_BASE, 1, &msg, MSG_OBJ_TYPE_TX);
     }
 
 	return 0;
